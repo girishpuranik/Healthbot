1b14d8fd38dba03e868756224d36ba931d26427f
Girish Shivkumar Puranik <girishpuranik28@gmail.com>
2022-02-22 23:54:28 +0530

    Add files via upload

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild QnABot.sln -p:DeployOnBuild=true -p:PublishProfile=.\azurehealthbot-bot-be9e-Web-Deploy.pubxml -p:Password=bgGwY63J6TM5n8hRyuzj9GlzXY1dgNvGmWhiCQcJqy7TligKrH2lMuSTpzrc
+
